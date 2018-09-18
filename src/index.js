16f3cedc3be9ea8ebb04cb92549797c91f87d1ea
lior cohen <liorco70@yahoo.com>
2018-09-18 20:52:16 +0200

    deleted redux stuff from index.js...

--- src/index.js
+++ src/index.js
@@ -4,15 +4,11 @@ import './index.css'
 import App from './App'
 import 'semantic-ui-css/semantic.min.css'
 import registerServiceWorker from './registerServiceWorker'
-import store from './redux/redux.js'
-import { Provider } from 'react-redux'
 import MainContextProvider from './context/MainContextProvider'
 ReactDOM.render(
-  <Provider store={store}>
-    <MainContextProvider>
-      <App />
-    </MainContextProvider>
-  </Provider>,
+  <MainContextProvider>
+    <App />
+  </MainContextProvider>,
   document.getElementById('root')
 )
 registerServiceWorker()
